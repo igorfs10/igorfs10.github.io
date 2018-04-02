76f5d7d074b6c8fd172026881a9f6538384c9a0c
igorfs10 <igorfs10@gmail.com>
2018-04-02 19:45:28 -0300

    CSS separado, a cpu pode ser O e X sempre começa

--- interfacejogodavelha.js
+++ interfacejogodavelha.js
@@ -1,31 +1,29 @@
-function mostrarVencedor(){ // Verifica se alguma linha de X ou O foi feita ou se todos os espacos foram usados para definir o resultado da partida e mostrar quem venceu
-	if((espaco[0]==="X" && espaco[3]==="X" && espaco[6]==="X") || (espaco[1]==="X" && espaco[4]==="X" && espaco[7]==="X") || (espaco[2]==="X" && espaco[5]==="X" && espaco[8]==="X") || (espaco[0]==="X" && espaco[1]==="X" && espaco[2]==="X") || (espaco[3]==="X" && espaco[4]==="X" && espaco[5]==="X") || (espaco[6]==="X" && espaco[7]==="X" && espaco[8]==="X") || (espaco[0]==="X" && espaco[4]==="X" && espaco[8]==="X") || (espaco[2]==="X" && espaco[4]==="X" && espaco[6]==="X")){
-		document.getElementById("status").innerHTML = "X ganhou";
-	}else if((espaco[0]==="O" && espaco[3]==="O" && espaco[6]==="O") || (espaco[1]==="O" && espaco[4]==="O" && espaco[7]==="O") || (espaco[2]==="O" && espaco[5]==="O" && espaco[8]==="O") || (espaco[0]==="O" && espaco[1]==="O" && espaco[2]==="O") || (espaco[3]==="O" && espaco[4]==="O" && espaco[5]==="O") || (espaco[6]==="O" && espaco[7]==="O" && espaco[8]==="O") || (espaco[0]==="O" && espaco[4]==="O" && espaco[8]==="O") || (espaco[2]==="O" && espaco[4]==="O" && espaco[6]==="O")){
-		document.getElementById("status").innerHTML = "O ganhou";
+// Verifica se alguma linha de X ou O foi feita ou se todos os espacos foram usados para mostrar quem venceu
+function mostrarVencedor(){
+	if((espaco[0] === jogador && espaco[3] === jogador && espaco[6] === jogador) || (espaco[1] === jogador && espaco[4] === jogador && espaco[7] === jogador) || (espaco[2] === jogador && espaco[5] === jogador && espaco[8] === jogador) || (espaco[0] === jogador && espaco[1] === jogador && espaco[2] === jogador) || (espaco[3] === jogador && espaco[4] === jogador && espaco[5] === jogador) || (espaco[6] === jogador && espaco[7] === jogador && espaco[8] === jogador) || (espaco[0] === jogador && espaco[4] === jogador && espaco[8] === jogador) || (espaco[2] === jogador && espaco[4] === jogador && espaco[6] === jogador)){
+		document.getElementById("status").innerHTML = jogador + " ganhou";
 	}else if (espaco[0] && espaco[1] && espaco[2] && espaco[3] && espaco[4] && espaco[5] && espaco[6] && espaco[7] && espaco[8]){
 		document.getElementById("status").innerHTML = "Empate";
 	}
 }
 
-function vezJogador(){ // Mostra quem esta jogando
+// Mostra quem esta jogando
+function vezJogador(){
 	if (jogando===true){
-		if(jogador===1){
-			document.getElementById("status").innerHTML = "Jogador O";
-		}else{
-			document.getElementById("status").innerHTML = "Jogador X";
-		}
+			document.getElementById("status").innerHTML = "Jogador " + jogador;
 	}else{
 		document.getElementById("status").innerHTML = "Escolha o jogo";
 	}
 }
 
-function atualizarQuadro(){ // Verifica as variaveis dos campos do jogo e faz com que apareça nos campos
+// Verifica as variaveis dos campos do jogo e faz com que apareça nos campos
+function atualizarQuadro(){
 	for(id = 0;id <= 8; id++){
 		document.getElementById(id).innerHTML = espaco[id]; // Escreve o que esta na variavel no quadro
 	}
 }
 
+// Escreve "X" ou "O" no espaco clicado
 function mostrarNoQuadro(id){
-	document.getElementById(id).innerHTML = espaco[id]; // Escreve "X" ou "O" no espaco clicado
+	document.getElementById(id).innerHTML = espaco[id];
 }
--- jogodavelha.js
+++ jogodavelha.js
@@ -1,11 +1,14 @@
 ﻿var espaco = ["", "", "", "", "", "", "", "", ""];
 var jogo = "";
 var jogando = false;
-var jogador = 0;
+var jogador = "X";
+var oponente = "O";
+var cpu = "O";
+var primeiroJogador = 0;
 var id = 0;
 
-
-function resetar(){ //Zera todas as variaveis e atualiza o quadro para forçar um jogo novo
+// Zera todas as variaveis e atualiza o quadro para forçar um jogo novo
+function resetar(){
 	espaco[0] = "";
 	espaco[1] = "";
 	espaco[2] = "";
@@ -17,175 +20,181 @@ function resetar(){ //Zera todas as variaveis e atualiza o quadro para forçar u
 	espaco[8] = "";
 	jogo = "";
 	jogando = false;
-	jogador = id = Math.floor(Math.random() * 10) + 1;
-	if(jogador <= 5){
-		jogador = 0;
+	primeiroJogador = Math.floor(Math.random() * 10) + 1;
+	jogador = "X";
+	oponente = "O";
+	if(primeiroJogador <= 5){
+		cpu = jogador;
 	}else{
-		jogador = 1;
+		cpu = oponente;
 	}
 	atualizarQuadro();
 }
 
-function verificarVencedor(){ // Verifica se alguma linha de X ou O foi feita ou se todos os espacos foram usados para definir o resultado da partida
-mostrarVencedor()
-	if(document.getElementById("status").innerHTML === "X ganhou" || document.getElementById("status").innerHTML === "O ganhou" || document.getElementById("status").innerHTML === "Empate"){
+// Verifica se alguma linha de X ou O foi feita ou se todos os espacos foram usados para definir o resultado da partida
+function verificarVencedor(){
+    mostrarVencedor();
+	if(document.getElementById("status").innerHTML === jogador + " ganhou" || document.getElementById("status").innerHTML === "Empate"){
 		jogando = false;
 	}
 }
 
-function turno(){ // Muda a variavel para ver quem vai jogar
-	if(jogador === 0){
-		jogador = 1;
+// Muda a variavel para a vez de quem vai jogar
+function turno(){
+	if(jogador === "X"){
+		jogador = "O";
+		oponente = "X";
 	}else{
-		jogador = 0;
+		jogador = "X";
+		oponente = "O"
 	}
 }
 
-function novoTurno(id){ // Realiza os passos repetitivos sempre que alguem joga
-	if(jogador===1){
-		espaco[id] = "O";
-	}else{
-		espaco[id] = "X";
-	}
+// Realiza os passos repetitivos sempre que alguem joga
+function novoTurno(id){
+	espaco[id] = jogador;
 	mostrarNoQuadro(id);
-	turno();
 	verificarVencedor();
+	turno();
 	if(jogando === true){
-		vezJogador();				
+		vezJogador();
 	}
 }
 
-function botaoJogo(tipoDeJogo){ // Botao para comecar o jogo
+// Sorteia um espaco, se sortear um espaco usado, sortea um novo
+function sortearEspaco(){
+	do{
+		id = Math.floor((Math.random() * 9) + 1) - 1;
+	}while(espaco[id] === jogador || espaco[id] === oponente);
+	return id;
+}
+
+// O computador joga de modo diferente dependendo do nivel
+function vezCPU(){
+	if(jogo === "facil"){
+		// Somente sorteia onde o computador irá jogar
+		jogar(sortearEspaco());
+	}else if(jogo === "medio"){
+		// Ele verifica se o oponente tem dois espacos proximos e coloca próximo para impedir que o oponente ganhe
+		if((espaco[1] === oponente && espaco[2] === oponente && espaco[0] === "") || (espaco[4] === oponente && espaco[8] === oponente && espaco[0] === "") || (espaco[3] === oponente && espaco[6] === oponente && espaco[0] === "")){
+			jogar(0);
+		}else if((espaco[0] === oponente && espaco[2] === oponente && espaco[1] === "") || (espaco[4] === oponente && espaco[7] === oponente && espaco[1] === "")){
+			jogar(1);
+		}else if((espaco[0] === oponente && espaco[1] === oponente && espaco[2] === "") || (espaco[5] === oponente && espaco[8] === oponente && espaco[2] === "") || (espaco[4] === oponente && espaco[6] === oponente && espaco[2] === "")){
+			jogar(2);
+		}else if((espaco[0] === oponente && espaco[6] === oponente && espaco[3] === "") || (espaco[4] === oponente && espaco[5] === oponente && espaco[3] === "")){
+			jogar(3);
+		}else if((espaco[0] === oponente && espaco[8] === oponente && espaco[4] === "") || (espaco[2] === oponente && espaco[6] === oponente && espaco[4] === "") || (espaco[1] === oponente && espaco[7] === oponente && espaco[4] === "") || (espaco[3] === oponente && espaco[5] === oponente && espaco[4] === "")){
+			jogar(4);
+		}else if((espaco[3] === oponente && espaco[4] === oponente && espaco[5] === "") || (espaco[2] === oponente && espaco[8] === oponente && espaco[5] === "")){
+			jogar(5);
+		}else if((espaco[0] === oponente && espaco[3] === oponente && espaco[6] === "") || (espaco[4] === oponente && espaco[2] === oponente && espaco[6] === "") || (espaco[7] === oponente && espaco[8] === oponente && espaco[6] === "")){
+			jogar(6);
+		}else if((espaco[1] === oponente && espaco[4] === oponente && espaco[7] === "") || (espaco[6] === oponente && espaco[8] === oponente && espaco[7] === "")){
+			jogar(7);
+		}else if((espaco[0] === oponente && espaco[4] === oponente && espaco[8] === "") || (espaco[6] === oponente && espaco[7] === oponente && espaco[8] === "") || (espaco[2] === oponente && espaco[5] === oponente && espaco[8] === "")){
+			jogar(8);
+		}else{
+			jogar(sortearEspaco());
+		}
+	}else if(jogo === "dificil"){
+		// Verifica se tem dois espacos e marca no proximo para ganhar
+		if((espaco[1] === jogador && espaco[2] === jogador && espaco[0] === "") || (espaco[4] === jogador && espaco[8] === jogador && espaco[0] === "") || (espaco[3] === jogador && espaco[6] === jogador && espaco[0] === "")){
+			jogar(0);
+		}else if((espaco[0] === jogador && espaco[2] === jogador && espaco[1] === "") || (espaco[4] === jogador && espaco[7] === jogador && espaco[1] === "")){
+			jogar(1);
+		}else if((espaco[0] === jogador && espaco[1] === jogador && espaco[2] === "") || (espaco[5] === jogador && espaco[8] === jogador && espaco[2] === "") || (espaco[4] === jogador && espaco[6] === jogador && espaco[2] === "")){
+			jogar(2);
+		}else if((espaco[0] === jogador && espaco[6] === jogador && espaco[3] === "") || (espaco[4] === jogador && espaco[5] === jogador && espaco[3] === "")){
+			jogar(3);
+		}else if((espaco[0] === jogador && espaco[8] === jogador && espaco[4] === "") || (espaco[2] === jogador && espaco[6] === jogador && espaco[4] === "") || (espaco[1] === jogador && espaco[7] === jogador && espaco[4] === "") || (espaco[3] === jogador && espaco[5] === jogador && espaco[4] === "")){
+			jogar(4);
+		}else if((espaco[3] === jogador && espaco[4] === jogador && espaco[5] === "") || (espaco[2] === jogador && espaco[8] === jogador && espaco[5] === "")){
+			jogar(5);
+		}else if((espaco[0] === jogador && espaco[3] === jogador && espaco[6] === "") || (espaco[4] === jogador && espaco[2] === jogador && espaco[6] === "") || (espaco[7] === jogador && espaco[8] === jogador && espaco[6] === "")){
+			jogar(6);
+		}else if((espaco[1] === jogador && espaco[4] === jogador && espaco[7] === "") || (espaco[6] === jogador && espaco[8] === jogador && espaco[7] === "")){
+			jogar(7);
+		}else if((espaco[0] === jogador && espaco[4] === jogador && espaco[8] === "") || (espaco[6] === jogador && espaco[7] === jogador && espaco[8] === "") || (espaco[2] === jogador && espaco[5] === jogador && espaco[8] === "")){
+			jogar(8);
+		// Verifica se o oponente tem dois espacos e impede ele de ganhar
+		}else if((espaco[1] === oponente && espaco[2] === oponente && espaco[0] === "") || (espaco[4] === oponente && espaco[8] === oponente && espaco[0] === "") || (espaco[3] === oponente && espaco[6] === oponente && espaco[0] === "")){
+			jogar(0);
+		}else if((espaco[0] === oponente && espaco[2] === oponente && espaco[1] === "") || (espaco[4] === oponente && espaco[7] === oponente && espaco[1] === "")){
+			jogar(1);
+		}else if((espaco[0] === oponente && espaco[1] === oponente && espaco[2] === "") || (espaco[5] === oponente && espaco[8] === oponente && espaco[2] === "") || (espaco[4] === oponente && espaco[6] === oponente && espaco[2] === "")){
+			jogar(2);
+		}else if((espaco[0] === oponente && espaco[6] === oponente && espaco[3] === "") || (espaco[4] === oponente && espaco[5] === oponente && espaco[3] === "")){
+			jogar(3);
+		}else if((espaco[0] === oponente && espaco[8] === oponente && espaco[4] === "") || (espaco[2] === oponente && espaco[6] === oponente && espaco[4] === "") || (espaco[1] === oponente && espaco[7] === oponente && espaco[4] === "") || (espaco[3] === oponente && espaco[5] === oponente && espaco[4] === "")){
+			jogar(4);
+		}else if((espaco[3] === oponente && espaco[4] === oponente && espaco[5] === "") || (espaco[2] === oponente && espaco[8] === oponente && espaco[5] === "")){
+			jogar(5);
+		}else if((espaco[0] === oponente && espaco[3] === oponente && espaco[6] === "") || (espaco[4] === oponente && espaco[2] === oponente && espaco[6] === "") || (espaco[7] === oponente && espaco[8] === oponente && espaco[6] === "")){
+			jogar(6);
+		}else if((espaco[1] === oponente && espaco[4] === oponente && espaco[7] === "") || (espaco[6] === oponente && espaco[8] === oponente && espaco[7] === "")){
+			jogar(7);
+		}else if((espaco[0] === oponente && espaco[4] === oponente && espaco[8] === "") || (espaco[6] === oponente && espaco[7] === oponente && espaco[8] === "") || (espaco[2] === oponente && espaco[5] === oponente && espaco[8] === "")){
+			jogar(8);
+		// Tentar fechar o jogador se ele comecar
+		}else if((espaco[0] === oponente || espaco[1] === oponente ||espaco[2] === oponente || espaco[3] === oponente ||espaco[5] === oponente ||espaco[6] === oponente || espaco[7] === oponente ||espaco[8] === oponente) && espaco[0]!=jogador && espaco[4] === ""){
+			jogar(4);
+		}else if(((espaco[1] === oponente && espaco[3] === oponente) || (espaco[1] === oponente && espaco[5] === oponente) || (espaco[3] === oponente && espaco[7] === oponente) || (espaco[5] === oponente && espaco[7] === oponente)) && espaco[4] === ""){
+			jogar(4);
+		}else if((espaco[1] === oponente || espaco[3] === oponente) && espaco[0] === ""){
+			jogar(0);
+		}else if((espaco[5] === oponente || espaco[7] === oponente) && espaco[8] === ""){
+			jogar(8);
+		}else if(espaco[1] === oponente && espaco[6] === oponente && espaco[4] === ""){
+			jogar(4);
+		}else if(espaco[4] === jogador && espaco[7] === ""){
+			jogar(7);
+		}else if(espaco[4] === jogador && espaco[1] === ""){
+			jogar(1);
+		// Se nao cair nos casos acima, continue jogando
+		}else if(espaco[0] === ""){
+			jogar(0);
+		}else if(espaco[8] === ""){
+			jogar(8);
+		}else if(espaco[4] === ""){
+			jogar(4);
+		}else if(espaco[2] === ""){
+			jogar(2);
+		}else if(espaco[1] === ""){
+			jogar(1);
+		}else if(espaco[7] === ""){
+			jogar(7);
+		}else if(espaco[6] === ""){
+			jogar(6);
+		}else if(espaco[3] === ""){
+			jogar(3);
+		}else if(espaco[5] === ""){
+			jogar(5);
+		}
+	}
+}
+
+// Botao para comecar o jogo
+function botaoTipoDeJogo(tipoDeJogo){
 	resetar();
 	jogo = tipoDeJogo;
 	jogando = true;
 	vezJogador();
-	if(jogador === 0 && (jogo === "facil" || jogo === "medio" || jogo === "dificil")){
-		CPU();
+	// O computador comeca se for "X"
+	if(cpu === "X" && (jogo === "facil" || jogo === "medio" || jogo === "dificil")){
+		vezCPU();
 	}
 }
 
-function sortearCPU(){
-	do{	// Sorteia outro número se o número sorteado ja estiver sendo usado
-		id = Math.floor((Math.random() * 9) + 1) - 1;
-	}while(espaco[id] === "X" || espaco[id] === "O")
-	return id;
-}
-
-function CPU(){ // O Computador só jogar se a variável jogo for facil, medio ou dificil
-	if(jogo === "facil"){		// Somente sorteia onde o computador irá jogar
-		botao(sortearCPU());
-		
-	}else if(jogo === "medio"){ // Ele verifica se o oponente tem dois espacos proximos e coloca "X" próximo para impedir que o oponente ganhe
-		if((espaco[1]==="O" && espaco[2]==="O" && espaco[0]==="") || (espaco[4]==="O" && espaco[8]==="O" && espaco[0]==="") || (espaco[3]==="O" && espaco[6]==="O" && espaco[0]==="")){
-			botao(0);
-		}else if((espaco[0]==="O" && espaco[2]==="O" && espaco[1]==="") || (espaco[4]==="O" && espaco[7]==="O" && espaco[1]==="")){
-			botao(1);
-		}else if((espaco[0]==="O" && espaco[1]==="O" && espaco[2]==="") || (espaco[5]==="O" && espaco[8]==="O" && espaco[2]==="") || (espaco[4]==="O" && espaco[6]==="O" && espaco[2]==="")){
-			botao(2);
-		}else if((espaco[0]==="O" && espaco[6]==="O" && espaco[3]==="") || (espaco[4]==="O" && espaco[5]==="O" && espaco[3]==="")){
-			botao(3);
-		}else if((espaco[0]==="O" && espaco[8]==="O" && espaco[4]==="") || (espaco[2]==="O" && espaco[6]==="O" && espaco[4]==="") || (espaco[1]==="O" && espaco[7]==="O" && espaco[4]==="") || (espaco[3]==="O" && espaco[5]==="O" && espaco[4]==="")){
-			botao(4);
-		}else if((espaco[3]==="O" && espaco[4]==="O" && espaco[5]==="") || (espaco[2]==="O" && espaco[8]==="O" && espaco[5]==="")){
-			botao(5);
-		}else if((espaco[0]==="O" && espaco[3]==="O" && espaco[6]==="") || (espaco[4]==="O" && espaco[2]==="O" && espaco[6]==="") || (espaco[7]==="O" && espaco[8]==="O" && espaco[6]==="")){
-			botao(6);
-		}else if((espaco[1]==="O" && espaco[4]==="O" && espaco[7]==="") || (espaco[6]==="O" && espaco[8]==="O" && espaco[7]==="")){
-			botao(7);
-		}else if((espaco[0]==="O" && espaco[4]==="O" && espaco[8]==="") || (espaco[6]==="O" && espaco[7]==="O" && espaco[8]==="") || (espaco[2]==="O" && espaco[5]==="O" && espaco[8]==="")){
-			botao(8);
-		}else{
-			botao(sortearCPU());
-		}
-		
-	}else if(jogo === "dificil"){
-		// Verifica se tem dois espacos e marca no proximo para ganhar
-		if((espaco[1]==="X" && espaco[2]==="X" && espaco[0]==="") || (espaco[4]==="X" && espaco[8]==="X" && espaco[0]==="") || (espaco[3]==="X" && espaco[6]==="X" && espaco[0]==="")){
-			botao(0);
-		}else if((espaco[0]==="X" && espaco[2]==="X" && espaco[1]==="") || (espaco[4]==="X" && espaco[7]==="X" && espaco[1]==="")){
-			botao(1);
-		}else if((espaco[0]==="X" && espaco[1]==="X" && espaco[2]==="") || (espaco[5]==="X" && espaco[8]==="X" && espaco[2]==="") || (espaco[4]==="X" && espaco[6]==="X" && espaco[2]==="")){
-			botao(2);
-		}else if((espaco[0]==="X" && espaco[6]==="X" && espaco[3]==="") || (espaco[4]==="X" && espaco[5]==="X" && espaco[3]==="")){
-			botao(3);
-		}else if((espaco[0]==="X" && espaco[8]==="X" && espaco[4]==="") || (espaco[2]==="X" && espaco[6]==="X" && espaco[4]==="") || (espaco[1]==="X" && espaco[7]==="X" && espaco[4]==="") || (espaco[3]==="X" && espaco[5]==="X" && espaco[4]==="")){
-			botao(4);
-		}else if((espaco[3]==="X" && espaco[4]==="X" && espaco[5]==="") || (espaco[2]==="X" && espaco[8]==="X" && espaco[5]==="")){
-			botao(5);
-		}else if((espaco[0]==="X" && espaco[3]==="X" && espaco[6]==="") || (espaco[4]==="X" && espaco[2]==="X" && espaco[6]==="") || (espaco[7]==="X" && espaco[8]==="X" && espaco[6]==="")){
-			botao(6);
-		}else if((espaco[1]==="X" && espaco[4]==="X" && espaco[7]==="") || (espaco[6]==="X" && espaco[8]==="X" && espaco[7]==="")){
-			botao(7);
-		}else if((espaco[0]==="X" && espaco[4]==="X" && espaco[8]==="") || (espaco[6]==="X" && espaco[7]==="X" && espaco[8]==="") || (espaco[2]==="X" && espaco[5]==="X" && espaco[8]==="")){
-			botao(8);
-		// Verifica se o oponente tem dois espacos e impede ele de ganhar
-		}else if((espaco[1]==="O" && espaco[2]==="O" && espaco[0]==="") || (espaco[4]==="O" && espaco[8]==="O" && espaco[0]==="") || (espaco[3]==="O" && espaco[6]==="O" && espaco[0]==="")){
-			botao(0);
-		}else if((espaco[0]==="O" && espaco[2]==="O" && espaco[1]==="") || (espaco[4]==="O" && espaco[7]==="O" && espaco[1]==="")){
-			botao(1);
-		}else if((espaco[0]==="O" && espaco[1]==="O" && espaco[2]==="") || (espaco[5]==="O" && espaco[8]==="O" && espaco[2]==="") || (espaco[4]==="O" && espaco[6]==="O" && espaco[2]==="")){
-			botao(2);
-		}else if((espaco[0]==="O" && espaco[6]==="O" && espaco[3]==="") || (espaco[4]==="O" && espaco[5]==="O" && espaco[3]==="")){
-			botao(3);
-		}else if((espaco[0]==="O" && espaco[8]==="O" && espaco[4]==="") || (espaco[2]==="O" && espaco[6]==="O" && espaco[4]==="") || (espaco[1]==="O" && espaco[7]==="O" && espaco[4]==="") || (espaco[3]==="O" && espaco[5]==="O" && espaco[4]==="")){
-			botao(4);
-		}else if((espaco[3]==="O" && espaco[4]==="O" && espaco[5]==="") || (espaco[2]==="O" && espaco[8]==="O" && espaco[5]==="")){
-			botao(5);
-		}else if((espaco[0]==="O" && espaco[3]==="O" && espaco[6]==="") || (espaco[4]==="O" && espaco[2]==="O" && espaco[6]==="") || (espaco[7]==="O" && espaco[8]==="O" && espaco[6]==="")){
-			botao(6);
-		}else if((espaco[1]==="O" && espaco[4]==="O" && espaco[7]==="") || (espaco[6]==="O" && espaco[8]==="O" && espaco[7]==="")){
-			botao(7);
-		}else if((espaco[0]==="O" && espaco[4]==="O" && espaco[8]==="") || (espaco[6]==="O" && espaco[7]==="O" && espaco[8]==="") || (espaco[2]==="O" && espaco[5]==="O" && espaco[8]==="")){
-			botao(8);
-		// Tentar fechar o jogador se ele comecar
-		}else if((espaco[0]==="O" || espaco[1]==="O" ||espaco[2]==="O" || espaco[3]==="O" ||espaco[5]==="O" ||espaco[6]==="O" || espaco[7]==="O" ||espaco[8]==="O") && espaco[0]!="X" && espaco[4]===""){
-			botao(4);
-		}else if(((espaco[1]==="O" && espaco[3]==="O") || (espaco[1]==="O" && espaco[5]==="O") || (espaco[3]==="O" && espaco[7]==="O") || (espaco[5]==="O" && espaco[7]==="O")) && espaco[4]===""){
-			botao(4);
-		}else if((espaco[1]==="O" || espaco[3]==="O") && espaco[0]===""){
-			botao(0);
-		}else if((espaco[5]==="O" || espaco[7]==="O") && espaco[8]===""){
-			botao(8);
-		}else if(espaco[1]==="O" && espaco[6]==="O" && espaco[4]===""){
-			botao(4);
-		}else if(espaco[4]==="X" && espaco[7]===""){
-			botao(7);
-		}else if(espaco[4]==="X" && espaco[1]===""){
-			botao(1);
-		// Se nao cair nos casos acima, continue jogando
-		}else if(espaco[0]===""){ //Comeca pelo canto
-			botao(0);
-		}else if(espaco[8]===""){
-			botao(8);
-		}else if(espaco[4]===""){
-			botao(4);
-		}else if(espaco[2]===""){
-			botao(2);
-		}else if(espaco[1]===""){
-			botao(1);
-		}else if(espaco[7]===""){
-			botao(7);
-		}else if(espaco[6]===""){
-			botao(6);
-		}else if(espaco[3]===""){
-			botao(3);
-		}else if(espaco[5]===""){
-			botao(5);
-		}
-	}
-}
-
-function botao(id){ // Vai pegar o id do quadro que o jogador clicar
+// Vai pegar o id do quadro que o jogador clicar
+function jogar(id){
 	if(jogando === true){
-		if(espaco[id] === ""){ // Verifica se o espaco clicado ja foi usado
-			if(jogador === 0){ // Verifica quem está jogando
-				novoTurno(id);
-			}else{
-				novoTurno(id);
-				if(jogando === true && (jogo === "facil" || jogo === "medio" || jogo === "dificil")){
-					CPU(); // O Computador joga se estiver em uma partida contra a CPU
-				}
+		// Verifica se o espaco clicado ja foi usado
+		if(espaco[id] === ""){
+			novoTurno(id);
+			// O computador joga se estiver em uma partida contra a CPU
+			if(jogando === true && jogador === cpu && (jogo === "facil" || jogo === "medio" || jogo === "dificil")){
+				vezCPU();
 			}
 		}
 	}else{

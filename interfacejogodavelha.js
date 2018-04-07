8fd91daf70b9e91b07f0824067c7ddd36da37978
igorfs10 <igorfs10@gmail.com>
2018-04-06 22:42:47 -0300

    Muda a cor do quadro dependendo do jogador e
    aumenta o tamanho

--- interfacejogodavelha.js
+++ interfacejogodavelha.js
@@ -3,7 +3,7 @@ function mostrarVencedor(){
 	if((espaco[0] === jogador && espaco[3] === jogador && espaco[6] === jogador) || (espaco[1] === jogador && espaco[4] === jogador && espaco[7] === jogador) || (espaco[2] === jogador && espaco[5] === jogador && espaco[8] === jogador) || (espaco[0] === jogador && espaco[1] === jogador && espaco[2] === jogador) || (espaco[3] === jogador && espaco[4] === jogador && espaco[5] === jogador) || (espaco[6] === jogador && espaco[7] === jogador && espaco[8] === jogador) || (espaco[0] === jogador && espaco[4] === jogador && espaco[8] === jogador) || (espaco[2] === jogador && espaco[4] === jogador && espaco[6] === jogador)){
 		document.getElementById("status").innerHTML = jogador + " ganhou";
 		for(id = 0;id <= 8; id++){
-			document.getElementById(id).dataset.selecionado = 0;
+			document.getElementById(id).dataset.selecionado = "Desativado";
 		}
 	}else if (espaco[0] && espaco[1] && espaco[2] && espaco[3] && espaco[4] && espaco[5] && espaco[6] && espaco[7] && espaco[8]){
 		document.getElementById("status").innerHTML = "Empate";
@@ -15,7 +15,7 @@ function vezJogador(){
 	if (jogando===true){
 			document.getElementById("status").innerHTML = "Jogador " + jogador;
 	}else{
-		document.getElementById("status").innerHTML = "Escolha o jogo";
+		document.getElementById("status").innerHTML = "Escolha o modo de jogo";
 	}
 }
 
@@ -23,12 +23,17 @@ function vezJogador(){
 function limparQuadro(){
 	for(id = 0;id <= 8; id++){
 		document.getElementById(id).innerHTML = espaco[id];
-		document.getElementById(id).dataset.selecionado = 1;
+		document.getElementById(id).dataset.selecionado = jogador;
 	}
 }
 
-// Escreve "X" ou "O" no espaco clicado e desabilita o efeito da celula selecionada
+// Escreve "X" ou "O" no espaco clicado, desabilita o efeito da celula selecionada e muda a cor do efeito dependendo do jogado
 function mostrarNoQuadro(id){
 	document.getElementById(id).innerHTML = espaco[id];
-	document.getElementById(id).dataset.selecionado = 0;
+	document.getElementById(id).dataset.selecionado = "Desativado";
+	for(id = 0;id <= 8; id++){
+		if(document.getElementById(id).dataset.selecionado === jogador){
+			document.getElementById(id).dataset.selecionado = oponente;
+		}
+	}
 }

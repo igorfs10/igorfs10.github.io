d46a55cbd05ed4e389bc3e9976e237cdd378247a
igorfs10 <igorfs10@gmail.com>
2018-10-19 08:12:33 -0300

    arquivo com insert do banco

--- banco escola/select insert banco escola.sql
+++ banco escola/select insert banco escola.sql
@@ -20,3 +20,31 @@ WHERE aula.idProfessor = pro.idProfessor AND aula.idDisciplina = dis.idDisciplin
 SELECT alu.nome, tur.idTurma, ativ.idAtividade, ativ.nome
 FROM aluno AS alu, turma AS tur, atividade as ativ, aluno_atividade as aluativ
 WHERE alu.idTurma = tur.idTurma AND alu.idAluno = aluativ.idAluno AND ativ.idAtividade = aluativ.idAtividade;
+
+-- Insert Turma
+INSERT INTO turma (nome, serie, periodo)
+VALUES ("Z", 1, "Tarde");
+
+-- Insert Aluno
+INSERT INTO aluno (nome, endereco, telefone, email, nomeDaMae, idTurma)
+VALUES ('Igor','Rua Castelo Branco, 300','98745-8120','igor@gmail.com','Rosa',2);
+
+-- Insert Professor
+INSERT INTO professor (nome, endereco, telefone, email, nomeDaMae)
+VALUES ('Igor','Rua Castelo Branco, 300','98745-8120','igor@gmail.com','Rosa');
+
+--Insert Atividade
+INSERT INTO atividade (nome)
+VALUES ("Prova");
+
+--Insert Disciplina
+INSERT INTO disciplina (nome)
+VALUES ("Geografia");
+
+--Insert Aluno_atividade
+INSERT INTO aluno_atividade (idAluno, idAtividade, nota)
+VALUES (2, 3, 4);
+
+-- Insert turma_atividade_professor_disciplina
+INSERT INTO turma_atividade_professor_disciplina (idProfessor, idTurma, idAtividade, idDisciplina, horario)
+VALUES (1,2,3,4,"Segunda 18:00");
